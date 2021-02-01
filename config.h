73db0df22b24f4e044aaa00cf0e789ab1ca49075
Christian Lund Hansen <christianlundhans3n@gmail.com>
2021-02-01 14:22:22 +0100

    added pertag and gaps

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 20;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -110,7 +111,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_y,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
@@ -129,6 +130,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },*/
 	{ MODKEY,                       XK_Tab,    shiftview,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_Tab,    shiftview,      {.i = -1 } },
+	{ MODKEY,                       XK_o,      setgaps,        {.i = -10 } },
+	{ MODKEY,                       XK_i,      setgaps,        {.i = +10 } },
+	{ MODKEY, 		        XK_p,      setgaps,        {.i = 0 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
